88a392e97d944e9fbfeccd6925856fe3bc8b5da8
Tmmrvg <Oetom01@outlook.com>
2021-09-24 14:36:00 +0200

    Commit MoveScript

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float moveSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float dashForce = 5f;
+    
+    [SerializeField] private LayerMask whatIsPit;
+    
+    private PlayerInput _Input;
+    
+    private Rigidbody2D _Rigidbody2D;
+    
+    private void Start()
     {
-        
+        _Input = GetComponent<PlayerInput>();
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    private void FixedUpdate()
+    {
+        _Rigidbody2D.velocity = 
+            new Vector2(_Input.moveVector.x * moveSpeed, _Input.moveVector.y * moveSpeed);
+    }
+    
+    private void Dash()
+    {
+        if (_Input.downDash)
+        {
+            _Rigidbody2D.AddForce(Vector2.down * dashForce, ForceMode2D.Impulse);
+        }
     }
 }
+
+
+
