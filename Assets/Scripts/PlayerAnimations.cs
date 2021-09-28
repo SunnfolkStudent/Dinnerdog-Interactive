8c4908701fc9f30f835b194571e1912e3db893ef
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2021-09-28 14:22:11 +0200

    Playeranimations commit

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimations : MonoBehaviour
+public class NewPlayerAnimation : MonoBehaviour
 {
+    #region Variables
+    
+    private Animator _animator;
+    private PlayerInput _playerInput;
+    private PlayerMovement _movement;
+    private PlayerCollision _collision;
+
+    private Rigidbody2D _rigidbody2D;
+    
+    #endregion
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _playerInput = GetComponent<PlayerInput>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _movement = GetComponent<PlayerMovement>();
+        _collision = GetComponent<PlayerCollision>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_playerInput.moveVector.x != 0)
+        {
+            transform.localScale = new Vector2(_playerInput.moveVector.x, 1f);
+        }
         
+        if (_playerInput.moveVector.y > 0)
+        {
+            _animator.Play("Player_Up");
+        }
+        else if (_playerInput.moveVector.y < 0)
+        {
+            _animator.Play("Player_Down");
+        }
+        else
+        {
+            _animator.Play("Idle");
+        }
+        
+        if (_playerInput.moveVector.x !=0)
+        {
+            _animator.Play("Player_Run");
+        }
+        else
+        {
+            _animator.Play("Idle");
+        }
     }
 }
