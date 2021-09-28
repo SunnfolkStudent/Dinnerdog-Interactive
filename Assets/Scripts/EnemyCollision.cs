f42c27e124ef5ae32fb48349c1b18e949e054f3b
Arctie <Arctiemail@gmail.com>
2021-09-28 14:01:06 +0200

    comitt

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -1,2 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Managers;
+using UnityEngine;
 
+public class EnemyCollision : MonoBehaviour
+{
+   private PlayerCollision _enemyCollision;
+   private EnemyHealth _enemyHealth;
+   
+   
+   private void Start()
+   {
+      _enemyCollision = GetComponent<PlayerCollision>();
+      _enemyHealth = GetComponent<EnemyHealth>();
+   }
 
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.CompareTag("Pit"))
+      {
+         _enemyHealth.EnemyTakeDamage(100);
+      }
+   }
+}
