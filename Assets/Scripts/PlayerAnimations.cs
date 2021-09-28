a5063909952ef3b98bfc14f6434ecc80132e775d
Alucardud <simenbasteson@hotmail.com>
2021-09-28 17:36:15 +0200

    PlayerFab

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewPlayerAnimation : MonoBehaviour
+public class PlayerAnimations : MonoBehaviour
 {
     #region Variables
     
