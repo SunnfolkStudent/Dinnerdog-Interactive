47e5e31e0a382d2521804b231db31229ac88bbde
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2021-09-29 10:28:29 +0200

    Improved healthmanager

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Managers
 {
@@ -7,26 +8,60 @@ namespace Managers
     {
         public static int lives = 3;
         public int maxLives = 3;
-
+        public GameObject heart3;
+        public GameObject heart2;
+        public GameObject heart1;
         
+
         private void Awake()
         {
             lives = maxLives;
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(true);
         }
-        
+
         public static void SetLives(int value)
         {
             lives = value;
         }
-        
+
         public static void AddLives()
         {
             lives++;
         }
-        
+
         public void ReduceLives()
         {
             lives--;
+        }
+
+        private void Update()
+        {
+            if (lives <= 3)
+            {
+                heart1.SetActive(true);
+                heart2.SetActive(true);
+                heart3.SetActive(true);
+            }
+            else if (lives == 2)
+            {
+                heart1.SetActive(true);
+                heart2.SetActive(true);
+                heart3.SetActive(false);
+            }
+            else if (lives == 1)
+            {
+                heart1.SetActive(true);
+                heart2.SetActive(false);
+                heart3.SetActive(false);
+            }
+            else if (lives < 1)
+            {
+                heart1.SetActive(false);
+                heart2.SetActive(false);
+                heart3.SetActive(false);
+            }
         }
     }
 }
