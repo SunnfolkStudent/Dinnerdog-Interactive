99c60ff35ba24de657106efbaba0dfdb38521328
Alucardud <simenbasteson@hotmail.com>
2021-09-29 19:15:02 +0200

    yes

--- Assets/Scripts/signSystem.cs
+++ Assets/Scripts/signSystem.cs
@@ -20,7 +20,6 @@ public class signSystem : MonoBehaviour
             text.SetActive(true);
         }
     }
-
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
