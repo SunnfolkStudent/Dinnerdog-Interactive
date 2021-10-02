9cd20ef0a6e1204f9271510d46668123c6f3ce93
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2021-10-02 11:21:22 +0200

    Score counter, smaller hitbox

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Managers
+{
+    public class ScoreManager: MonoBehaviour
+    {
+        public TMP_Text _tmpText;
+
+        private void Start()
+        {
+            _tmpText.text = "Food Collected: " + PlayerInteract.score.ToString() + "/3";
+        }
+
+        public void SetScoreUI()
+        {
+            if (PlayerInteract.score >=3)
+            {
+                _tmpText.text = "Return to kitchen";
+            }
+            else
+            {
+                _tmpText.text = "Food Collected: " + PlayerInteract.score.ToString() + "/3";
+            }
+        }
+
+        private void Update()
+        {
+            SetScoreUI();
+        }
+    }
+}
